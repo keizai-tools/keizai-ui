3c635b479541be4c855cb9463a69a1b7d3e46be8
Ivan Tammaro <tammaroivan@gmail.com>
2023-10-05 09:02:21 +1100

    Update husky

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,12 +1,8 @@
 import { Outlet } from 'react-router-dom';
 
-import Navbar from '@components/navbar/Navbar';
-
 export default function Root() {
 	return (
 		<>
-			<Navbar />
-			aas
 			<div id="pages">
 				<Outlet />
 			</div>
