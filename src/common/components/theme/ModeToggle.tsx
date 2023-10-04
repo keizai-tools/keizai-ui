f409513d56d3e073d611f185323f3cf2fe587b27
github_fedeesti <fede.estigarribia@hotmail.com>
2023-10-04 11:35:07 -0300

    Add dark/light theme

---
+++ src/common/components/theme/ModeToggle.tsx
@@ -0,0 +1,52 @@
+import { Button } from '@common/components/ui/button';
+import { Moon, Sun } from 'lucide-react';
+
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from '@/common/components/ui/dropdown-menu';
+import { useTheme } from '@/contexts/ThemeContext';
+
+export function ModeToggle() {
+	const { setTheme } = useTheme();
+
+	return (
+		<DropdownMenu data-test="theme-container">
+			<DropdownMenuTrigger asChild>
+				<Button variant="outline" size="icon" data-test="theme-dropdown-btn">
+					<Sun
+						className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+						data-test="theme-dropdown-img-sun"
+					/>
+					<Moon
+						className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+						data-test="theme-dropdown-img-moon"
+					/>
+					<span className="sr-only">Toggle theme</span>
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent align="end" data-test="theme-dropdown-container">
+				<DropdownMenuItem
+					onClick={() => setTheme('light')}
+					data-test="theme-dropdown-light"
+				>
+					Light
+				</DropdownMenuItem>
+				<DropdownMenuItem
+					onClick={() => setTheme('dark')}
+					data-test="theme-dropdown-dark"
+				>
+					Dark
+				</DropdownMenuItem>
+				<DropdownMenuItem
+					onClick={() => setTheme('system')}
+					data-test="theme-dropdown-system"
+				>
+					System
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+}
