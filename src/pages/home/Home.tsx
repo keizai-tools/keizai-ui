e74dc4627ab9ca9cea6b5ae6190db561bc696d47
github_fedeesti <fede.estigarribia@hotmail.com>
2023-10-04 15:14:29 -0300

    Add Collections component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,9 @@
+import Collections from '@/common/components/Collections/Collections';
+
 export default function Home() {
 	return (
-		<div className="flex items-center justify-center h-screen">
-			{/* data-test? https://docs.cypress.io/guides/references/best-practices */}
-			<h1 className="text-3xl font-bold underline" data-test="home-msg">
-				This is the homepage!
-			</h1>
-		</div>
+		<main className="flex flex-col flex-1  justify-between">
+			<Collections />
+		</main>
 	);
 }
