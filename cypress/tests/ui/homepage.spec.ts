6481be0123a2af89ad72904637160874519068d2
Federico Estigarribia <60760903+fedeesti@users.noreply.github.com>
2023-10-05 15:40:50 -0300

    Feature/collections (#3)
    # Summary
    
    - Add Collections component
    - Add `accordion` component to deploy on Collections
    
    # Details
    
    Implement basic collections pagebar
    
    # Evidence
    
    
    ![keizai-collections](https://github.com/keizai-tools/keizai-ui/assets/60760903/81c75561-428f-4f95-a9f0-e30ebc1fa3d0)

--- cypress/tests/ui/homepage.spec.ts
+++ cypress/tests/ui/homepage.spec.ts
@@ -1,28 +1,32 @@
-// https://docs.cypress.io/guides/overview/why-cypress
 describe('Home page', () => {
 	beforeEach(() => {
 		cy.visit('/');
 	});
 
-	it('Should show a title', () => {
-		cy.getBySel('home-msg')
+	it('Should show a Collections component', () => {
+		cy.getBySel('collections-container').should('exist').and('be.visible');
+		cy.getBySel('collections-header').should('exist').and('be.visible');
+		cy.getBySel('collections-header-title')
 			.should('be.visible')
-			.and('have.text', 'Keizai Homes');
+			.and('have.text', 'Collections');
+		cy.getBySel('collections-header-btn-new')
+			.should('be.visible')
+			.and('have.text', 'New');
+		cy.getBySel('collections-header-btn-import')
+			.should('be.visible')
+			.and('have.text', 'Import');
+		cy.getBySel('collections-accordion-container')
+			.should('exist')
+			.and('be.visible');
 	});
 	it('Should redirect to the Home page when navigating to other paths', () => {
 		cy.visit('/aaa');
-		cy.getBySel('home-msg')
-			.should('be.visible')
-			.and('have.text', 'Keizai Homes');
+		cy.getBySel('home-page-container').should('be.visible');
 
 		cy.visit('/bbb');
-		cy.getBySel('home-msg')
-			.should('be.visible')
-			.and('have.text', 'Keizai Homes');
+		cy.getBySel('home-page-container').should('be.visible');
 
 		cy.visit('/ccc');
-		cy.getBySel('home-msg')
-			.should('be.visible')
-			.and('have.text', 'Keizai Homes');
+		cy.getBySel('home-page-container').should('be.visible');
 	});
 });
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,12 @@
+import Collections from '@/common/components/Collections/Collections';
+
 export default function Home() {
 	return (
 		<main
-			className="flex flex-col flex-1 items-center justify-between p-4 text-primary"
-			data-test="home-msg"
+			className="flex flex-col flex-1  justify-between"
+			data-test="home-page-container"
 		>
-			Keizai Homes
+			<Collections />
 		</main>
 	);
 }
