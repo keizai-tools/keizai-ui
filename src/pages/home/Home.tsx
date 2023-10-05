7fcb49ad457b2f7cca68d5281762d5e96eeb56af
github_fedeesti <fede.estigarribia@hotmail.com>
2023-10-05 10:44:48 -0300

    Add tabs on the Home Page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,69 @@
+import Terminal from '@/common/components/ui/Terminal';
+import {
+	Tabs,
+	TabsContent,
+	TabsList,
+	TabsTrigger,
+} from '@/common/components/ui/tabs';
+
+const tabs: Record<string, string> = {
+	functions: 'Functions',
+	authorizations: 'Authorization',
+	preInvocateScript: 'Pre-invocate script',
+	tests: 'Tests',
+	events: 'Events',
+};
+
 export default function Home() {
 	return (
-		<div className="flex items-center justify-center h-screen">
-			{/* data-test? https://docs.cypress.io/guides/references/best-practices */}
-			<h1 className="text-3xl font-bold underline" data-test="home-msg">
-				This is the homepage!
-			</h1>
-		</div>
+		<main className="flex flex-1">
+			<div className="flex flex-col justify-between w-full gap-7">
+				<div className="flex flex-col p-3 w-full gap-7">
+					<Tabs
+						defaultValue="functions"
+						className=""
+						data-test="tabs-container"
+					>
+						<TabsList
+							className="bg-white text-black dark:bg-background dark:text-white"
+							data-test="tabs-list-container"
+						>
+							{Object.keys(tabs).map((tab) => (
+								<TabsTrigger
+									key={tab}
+									value={tab}
+									className="data-[state=active]:bg-background data-[state=active]:dark:bg-white data-[state=active]:text-white data-[state=active]:dark:text-black  rounded-md"
+								>
+									{tabs[tab]}
+								</TabsTrigger>
+							))}
+						</TabsList>
+						<TabsContent value="functions">
+							<div
+								className="flex justify-center mt-36 flex-1 gap-8"
+								data-test="tabs-content-container"
+							>
+								<img
+									src="/moon.svg"
+									alt="Load contract image"
+									width={300}
+									height={300}
+									loading="eager"
+									data-test="tabs-content-contract-img"
+								/>
+								<div
+									className="flex flex-col justify-center text-primary font-black text-6xl"
+									data-test="tabs-content-contract-text"
+								>
+									<h2>Let&apos;s Load</h2>
+									<h2>Your Contract</h2>
+								</div>
+							</div>
+						</TabsContent>
+					</Tabs>
+				</div>
+				<Terminal />
+			</div>
+		</main>
 	);
 }
