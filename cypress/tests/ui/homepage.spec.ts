5a79b40919a36dc9339db8c7dfe13feec591ac09
Federico Estigarribia <60760903+fedeesti@users.noreply.github.com>
2023-10-05 16:26:55 -0300

    Feature/bread input terminal (#4)
    # Summary
    
    - Add Breadcrumb and ContractInput components
    - Add `select` and `input` components
    - Add tests to Breadcrumb and ContractInput
    
    # Details
    
    Implement Breadcrumb and ContractInput components.
    I had to add cypress-real-events because I couldn't test the tooltip.
    
    # Evidence
    
    
    ![bread-input](https://github.com/keizai-tools/keizai-ui/assets/60760903/8b1cc16f-8cbc-4c6b-96fb-d55bf1a36acb)
    
    ![image](https://github.com/keizai-tools/keizai-ui/assets/60760903/d45ed43d-05ed-4787-8587-6a9f17e51401)
    
    # Reference
    
    [cypress-real-events](https://github.com/dmtrKovalenko/cypress-real-events#cyrealhover)

--- cypress/tests/ui/homepage.spec.ts
+++ cypress/tests/ui/homepage.spec.ts
@@ -3,6 +3,38 @@ describe('Home page', () => {
 		cy.visit('/');
 	});
 
+	it('Should show a Breadcrumb', () => {
+		const contractName = 'Counter contract / Basic use case /';
+
+		cy.getBySel('breadcrumb-container').should('exist').and('be.visible');
+		cy.getBySel('breadcrumb-contract-name').should(
+			'contain.text',
+			contractName,
+		);
+		cy.getBySel('breadcrumb-contract-invocation-name').should(
+			'contain.text',
+			'Get current counter',
+		);
+	});
+	it('Should show a contract input', () => {
+		const contractId =
+			'f47e3e34187dc84aa9ff41108082d289cdf6e40720cdfba8fcd9974369b9d32e';
+
+		cy.getBySel('contract-input-container').should('exist').and('be.visible');
+		cy.getBySel('contract-input-network')
+			.should('be.visible')
+			.and('have.text', 'FUTURENET');
+		cy.getBySel('input-contract-name')
+			.should('be.visible')
+			.and('have.value', contractId);
+		cy.getBySel('contract-input-btn-load')
+			.should('be.visible')
+			.and('have.text', 'LOAD');
+		cy.getBySel('contract-input-btn-load').realHover();
+		cy.getBySel('contract-input-btn-load-tooltip')
+			.should('be.visible')
+			.and('contain', 'Coming soon');
+    });
 	it('Should show a Collections component', () => {
 		cy.getBySel('collections-container').should('exist').and('be.visible');
 		cy.getBySel('collections-header').should('exist').and('be.visible');
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,25 @@
+import Breadcrumb from '@/common/components/Breadcrumb/Breadcrumb';
 import Collections from '@/common/components/Collections/Collections';
+import ContractInput from '@/common/components/Input/ContractInput';
 
 export default function Home() {
 	return (
 		<main
-			className="flex flex-col flex-1  justify-between"
+			className="flex flex-col flex-1 justify-between"
 			data-test="home-page-container"
 		>
-			<Collections />
+			<div
+				className="flex flex-col p-3 w-full gap-7"
+				data-test="home-page-container"
+			>
+				<Collections />
+				<Breadcrumb
+					contractName="Counter contract"
+					folderName="Basic use case"
+					contractInvocationName="Get current counter"
+				/>
+				<ContractInput />
+			</div>
 		</main>
 	);
 }
