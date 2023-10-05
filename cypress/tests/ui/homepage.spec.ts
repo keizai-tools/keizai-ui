717f24c0bec6ca1990d6d09e5259bb2ea3ce32fd
github_fedeesti <fede.estigarribia@hotmail.com>
2023-10-05 14:59:32 -0300

    Fix tests when pulling the main branch

--- cypress/tests/ui/homepage.spec.ts
+++ cypress/tests/ui/homepage.spec.ts
@@ -18,21 +18,15 @@ describe('Home page', () => {
 		cy.getBySel('collections-accordion-container')
 			.should('exist')
 			.and('be.visible');
-    });
+	});
 	it('Should redirect to the Home page when navigating to other paths', () => {
 		cy.visit('/aaa');
-		cy.getBySel('home-msg')
-			.should('be.visible')
-			.and('have.text', 'Keizai Homes');
+		cy.getBySel('home-page-container').should('be.visible');
 
 		cy.visit('/bbb');
-		cy.getBySel('home-msg')
-			.should('be.visible')
-			.and('have.text', 'Keizai Homes');
+		cy.getBySel('home-page-container').should('be.visible');
 
 		cy.visit('/ccc');
-		cy.getBySel('home-msg')
-			.should('be.visible')
-			.and('have.text', 'Keizai Homes');
+		cy.getBySel('home-page-container').should('be.visible');
 	});
 });
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,7 +2,10 @@ import Collections from '@/common/components/Collections/Collections';
 
 export default function Home() {
 	return (
-		<main className="flex flex-col flex-1  justify-between">
+		<main
+			className="flex flex-col flex-1  justify-between"
+			data-test="home-page-container"
+		>
 			<Collections />
 		</main>
 	);
