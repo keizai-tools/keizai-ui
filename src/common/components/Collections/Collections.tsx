ed5a4d48ce20a13a9543c79a5accec3ef1b54199
github_fedeesti <fede.estigarribia@hotmail.com>
2023-10-06 16:54:26 -0300

    Add Folder and MoreOptions under Collections

--- src/common/components/Collections/Collections.tsx
+++ src/common/components/Collections/Collections.tsx
@@ -1,5 +1,3 @@
-import { Code } from 'lucide-react';
-
 import {
 	Accordion,
 	AccordionContent,
@@ -7,21 +5,26 @@ import {
 	AccordionTrigger,
 } from '../ui/accordion';
 import { Button } from '../ui/button';
+import Folder from './Folder';
+import MoreOptions from './MoreOptions';
 
-const contractInvocations = [
-	'Get current counter',
-	'Increase counter',
-	'Decrease counter',
-];
+import { useCollections } from '@/providers/CollectionsProvider';
 
 const Collections = () => {
+	const {
+		collections,
+		addCollection,
+		removeCollection,
+		addFolderToCollection,
+		removeFolderFromCollection,
+	} = useCollections();
 	return (
 		<div
-			className="w-[300px] flex-col border-r dark:border-r-border h-full px-3 py-1"
+			className="w-[300px] flex-col border-r dark:border-r-border h-full px-3 py-1 gap-4"
 			data-test="collections-container"
 		>
 			<div
-				className="flex items-center  justify-between"
+				className="flex items-center justify-between"
 				data-test="collections-header"
 			>
 				<h4 className="text-lg font-bold" data-test="collections-header-title">
@@ -30,7 +33,10 @@ const Collections = () => {
 				<div className="">
 					<Button
 						variant="secondary"
-						className="text-xs px-2 py-1 h-auto "
+						className="text-xs px-2 py-1 h-auto"
+						onClick={() =>
+							addCollection(`New collection ${collections.length + 1}`)
+						}
 						data-test="collections-header-btn-new"
 					>
 						New
@@ -39,6 +45,7 @@ const Collections = () => {
 						variant="link"
 						className="p-2 text-foreground"
 						data-test="collections-header-btn-import"
+						disabled
 					>
 						Import
 					</Button>
@@ -47,24 +54,61 @@ const Collections = () => {
 			<Accordion
 				type="multiple"
 				className="w-full"
-				defaultValue={['folder-id']}
 				data-test="collections-accordion-container"
 			>
-				<AccordionItem value="folder-id" className="border-none">
-					<AccordionTrigger>Basic use case</AccordionTrigger>
-					<AccordionContent>
-						<div className="flex flex-col gap-2 ml-5">
-							{contractInvocations.map((invocation) => (
-								<div key={invocation} className="flex items-center gap-1">
-									<Code height={16} className="text-neutral-500" />
-									<Button variant="link" className="p-0 h-auto text-foreground">
-										{invocation}
-									</Button>
+				{collections.map((collection) => (
+					<AccordionItem
+						key={collection.id}
+						value={collection.id}
+						className="border-none"
+						data-test="collections-accordion-item"
+					>
+						<div className="flex justify-between items-center">
+							<AccordionTrigger
+								className="h-10 w-full"
+								data-test="collection-item-btn"
+							>
+								<div className="flex justify-between items-center w-full group">
+									<span data-test="collection-item-name">
+										{collection.name}
+									</span>
 								</div>
-							))}
+							</AccordionTrigger>
+							<MoreOptions
+								onAddFolder={() =>
+									addFolderToCollection(collection.id, 'New Folder')
+								}
+								// eslint-disable-next-line @typescript-eslint/no-empty-function
+								onClickEdit={() => {}}
+								onClickDelete={() => removeCollection(collection.id)}
+							/>
 						</div>
-					</AccordionContent>
-				</AccordionItem>
+						<AccordionContent>
+							<div className="flex flex-col gap-2 ml-5">
+								{collection.folders.length ? (
+									<div className="flex flex-col ml-5 text-slate-400">
+										{collection.folders.map((folder) => (
+											<Folder
+												key={folder.id}
+												folder={folder}
+												onRemove={() =>
+													removeFolderFromCollection(collection.id, folder.id)
+												}
+											/>
+										))}
+									</div>
+								) : (
+									<span
+										className="text-xs text-slate-400 ml-7"
+										data-test="collection-empty-folders"
+									>
+										No folders
+									</span>
+								)}
+							</div>
+						</AccordionContent>
+					</AccordionItem>
+				))}
 			</Accordion>
 		</div>
 	);
