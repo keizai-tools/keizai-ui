3b4b27b578eecf0f1b7224e999233974fb0a65ee
github_fedeesti <fede.estigarribia@hotmail.com>
2023-10-06 18:24:49 -0300

    Add styles in Sidebar

--- src/common/components/sidebar/Sidebar.tsx
+++ src/common/components/sidebar/Sidebar.tsx
@@ -1,14 +1,14 @@
 import { Copy } from 'lucide-react';
 import { Link, useLocation } from 'react-router-dom';
 
-import { ModeToggle } from '../theme/ModeToggle';
+import { ToggleThemeButton } from '../theme/ToggleThemeButton';
 
 function Sidebar() {
 	const location = useLocation();
 	const currentRoute = location.pathname;
 	return (
 		<div
-			className="h-screen w-[80px] flex flex-col items-center justify-between bg-background border-r border-r-background-100 py-4"
+			className="h-screen w-[80px] flex flex-col items-center justify-between bg-foreground dark:bg-background border-r dark:border-r-border py-4"
 			data-test="sidebar-container"
 		>
 			<div className="flex flex-col items-center">
@@ -19,7 +19,7 @@ function Sidebar() {
 					alt="Keizai Logo"
 					data-test="sidebar-img"
 				/>
-				<div className="flex flex-col mt-4 text-neutral-0">
+				<div className="flex flex-col mt-4 items-center">
 					<Link
 						to="/"
 						data-test="sidebar-link"
@@ -31,7 +31,7 @@ function Sidebar() {
 					</Link>
 				</div>
 			</div>
-			<ModeToggle />
+			<ToggleThemeButton />
 		</div>
 	);
 }
