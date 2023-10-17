1e75f2f4341d12069f33632f683fcd2e134a7ca4
github_fedeesti <fede.estigarribia@hotmail.com>
2023-10-17 16:21:38 -0300

    Change Providers location

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -2,17 +2,14 @@ import { Outlet } from 'react-router-dom';
 
 import { AppLoader } from '@/common/components/Loader/AppLoader';
 import Sidebar from '@/common/components/sidebar/Sidebar';
-import Providers from '@/providers/Providers';
 
 export default function Root() {
 	return (
-		<Providers>
-			<main className="flex min-h-screen bg-background">
-				<Sidebar />
-				<AppLoader>
-					<Outlet />
-				</AppLoader>
-			</main>
-		</Providers>
+		<main className="flex min-h-screen bg-background">
+			<Sidebar />
+			<AppLoader>
+				<Outlet />
+			</AppLoader>
+		</main>
 	);
 }
