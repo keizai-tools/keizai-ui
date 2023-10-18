8a20805faae3f423900e9ff2adc353de81c9c5e2
Ivan Tammaro <tammaroivan@gmail.com>
2023-10-18 23:24:34 +1100

    Remove unnecesary console.log

--- src/common/hooks/useAxios.ts
+++ src/common/hooks/useAxios.ts
@@ -3,15 +3,13 @@ import axios from 'axios';
 import { useAuth } from '@/services/auth/hook/useAuth';
 
 const useAxios = () => {
-	const { user, isLoading, isAuthenticated } = useAuth();
+	const { user } = useAuth();
 
 	const instance = axios.create({
 		baseURL: import.meta.env.VITE_URL_API_BASE,
 	});
 	instance.interceptors.request.use(
 		(config) => {
-			console.log({ user, isLoading, isAuthenticated });
-
 			if (user?.accessToken) {
 				config.headers.Authorization = `Bearer ${user.accessToken}`;
 			}
