76673d234d88e6f1c196b67b5e5f650715b0c391
Ivan Tammaro <tammaroivan@gmail.com>
2023-10-19 13:32:15 +1100

    Fix tests

--- src/common/components/Collections/CollectionPlaceholder.tsx
+++ src/common/components/Collections/CollectionPlaceholder.tsx
@@ -32,6 +32,7 @@ const CollectionPlaceholder = () => {
 				<Button
 					variant="ghost"
 					className="flex flex-col gap-4 border-dashed border-2 border-primary rounded-lg h-[200px] w-[300px] font-bold"
+					data-test="collections-header-btn-new"
 				>
 					<ListPlusIcon size={42} />
 					Create a new collection
