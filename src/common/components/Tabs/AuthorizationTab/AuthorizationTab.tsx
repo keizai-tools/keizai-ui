2ca253d72332359ddbec740e0c8609bef08f3714
Federico Estigarribia <60760903+fedeesti@users.noreply.github.com>
2023-10-31 12:36:38 -0300

    Feat/auth stellar (#34)

--- src/common/components/Tabs/AuthorizationTab/AuthorizationTab.tsx
+++ src/common/components/Tabs/AuthorizationTab/AuthorizationTab.tsx
@@ -1,60 +1,63 @@
-import debounce from 'lodash.debounce';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
+import CreateNewAccount from '../../auth/stellar/CreateNewAccount';
+import ImportAccount from '../../auth/stellar/ImportAccount';
 import { Input } from '../../ui/input';
 
 import { useEditInvocationKeysMutation } from '@/common/api/invocations';
+import { IKeypair } from '@/services/stellar/domain/keypair';
 
 const AuthorizationTab = ({
 	invocationId,
 	defaultValues,
 }: {
 	invocationId: string;
-	defaultValues: { secretKey?: string; publicKey?: string };
+	defaultValues: IKeypair;
 }) => {
-	const { mutate: editKeys } = useEditInvocationKeysMutation();
-	const { register, reset, formState, watch } = useForm({
+	const { mutate: editKeys, data } = useEditInvocationKeysMutation();
+	const { register } = useForm({
 		defaultValues: {
 			secretKey: defaultValues.secretKey,
 			publicKey: defaultValues.publicKey,
 		},
 	});
-	const data = watch();
 
-	const debounceKeyPhrase = React.useMemo(
-		() =>
-			debounce((value) => {
-				editKeys({
-					id: invocationId,
-					secretKey: value.secretKey,
-					publicKey: value.publicKey,
-				});
-				reset({
-					secretKey: value.secretKey,
-					publicKey: value.publicKey,
-				});
-			}, 500),
-		[editKeys, invocationId, reset],
-	);
-
-	React.useEffect(() => {
-		if (formState.isDirty) {
-			debounceKeyPhrase(data);
-		}
-	}, [data, debounceKeyPhrase, formState.isDirty]);
+	const defaultKeys = React.useMemo(() => {
+		return {
+			secretKey: data?.secretKey || defaultValues.secretKey,
+			publicKey: data?.publicKey || defaultValues.publicKey,
+		};
+	}, [data, defaultValues]);
 
 	return (
-		<div className="flex flex-col gap-2 mt-7">
-			<div className="flex flex-col gap-1">
-				<span className="text-primary text-md font-semibold">Secret key</span>
-				<Input {...register('secretKey')} placeholder="S . . ." />
+		<section data-test="auth-tab-container">
+			<div
+				className="flex flex-col gap-2 p-12"
+				data-test="auth-tab-keypair-container"
+			>
+				<div className="flex flex-col gap-1" data-test="auth-tab-secret-key">
+					<span className="text-primary text-md font-semibold">Secret key</span>
+					<Input
+						{...register('secretKey')}
+						placeholder="S . . ."
+						value={defaultKeys.secretKey || ''}
+					/>
+				</div>
+				<div className="flex flex-col gap-1" data-test="auth-tab-public-key">
+					<span className="text-primary text-md font-semibold">Public key</span>
+					<Input
+						{...register('publicKey')}
+						placeholder="G . . ."
+						value={defaultKeys.publicKey || ''}
+					/>
+				</div>
 			</div>
-			<div className="flex flex-col gap-1">
-				<span className="text-primary text-md font-semibold">Secret key</span>
-				<Input {...register('publicKey')} placeholder="G . . ." />
+			<div className="flex justify-center gap-4 h-12 w-[90%] px-4">
+				<CreateNewAccount invocationId={invocationId} editKeys={editKeys} />
+				<ImportAccount invocationId={invocationId} editKeys={editKeys} />
 			</div>
-		</div>
+		</section>
 	);
 };
 
---
+++ src/common/components/auth/stellar/CreateNewAccount.tsx
@@ -0,0 +1,44 @@
+import { UseMutateFunction } from '@tanstack/react-query';
+
+import { Button } from '../../ui/button';
+
+import useStellar from '@/services/stellar/hook/useStellar';
+
+function CreateNewAccount({
+	invocationId,
+	editKeys,
+}: {
+	invocationId: string;
+	editKeys: UseMutateFunction<
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		any,
+		Error,
+		{
+			id: string;
+			secretKey?: string | undefined;
+			publicKey?: string | undefined;
+		},
+		unknown
+	>;
+}) {
+	const { createNewAccount } = useStellar();
+
+	const onCreateAccount = () => {
+		const keypair = createNewAccount();
+		if (keypair) {
+			editKeys({ id: invocationId, ...keypair });
+		}
+	};
+	return (
+		<Button
+			className="font-bold px-10 border-[3px] border-primary text-primary h-full hover:text-background hover:bg-primary"
+			variant="outline"
+			data-test="auth-stellar-create-account-btn"
+			onClick={onCreateAccount}
+		>
+			Generate new account
+		</Button>
+	);
+}
+
+export default CreateNewAccount;
