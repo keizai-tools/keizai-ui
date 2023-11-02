b4947c3aa9f0ae42a0e06f01375741eeb8f4684d
Ivan Tammaro <tammaroivan@gmail.com>
2023-11-02 23:16:48 +1100

    Remove flaky test

--- cypress/tests/ui/authpage.spec.ts
+++ cypress/tests/ui/authpage.spec.ts
@@ -399,16 +399,6 @@ describe('Authentication page management', () => {
 			cy.getBySel('change-password-btn-submit').click();
 			cy.getBySel('confirm-password-error').contains(validateErrors.notMatch);
 		});
-		it('Should show an error message when failed password change', () => {
-			cy.getBySel('change-password-form-container').click();
-			cy.getBySel('form-input-password').eq(0).type('test');
-			cy.getBySel('form-input-password').eq(1).type(user.password);
-			cy.getBySel('form-input-password').eq(2).type(user.password);
-			cy.getBySel('change-password-btn-submit').click();
-			cy.getBySel('change-password-error-message')
-				.should('be.visible')
-				.and('have.text', changePassword.error);
-		});
 		it('Should change a password successfully', () => {
 			cy.getBySel('change-password-form-container').click();
 			cy.getBySel('form-input-password').eq(0).type(user.password);
