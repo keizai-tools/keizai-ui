7c0aea1b34a81e85179e01596994010794cdc116
Federico Estigarribia <60760903+fedeesti@users.noreply.github.com>
2023-11-15 14:49:52 -0300

    Feat/folder test (#51)

---
+++ cypress/tests/ui/exceptions/contants.ts
@@ -0,0 +1,28 @@
+export const collectionId = '45cb5f2a-5c38-4cef-a0ac-bb292682c018';
+export const folders = {
+	folderId: 'c1f7a826-acdd-446b-97b2-1600fddc8023',
+	title: 'Folders',
+	loadingDescription: 'Loading folders...',
+	emptyFolderDescription: 'Create your first folder here',
+	dialog: {
+		create: {
+			title: 'New folder',
+			description: "Let's name your folder",
+			defaultValue: 'Folder',
+			btnText: 'Create',
+		},
+		delete: {
+			title: 'Are you sure?',
+			description:
+				'This will permanently delete your folder and all related invocations.',
+			btnCancelText: 'Cancel',
+			btnContinueText: 'Continue',
+		},
+		edit: {
+			title: 'Edit folder',
+			description: "Let's name your folder",
+			btnText: 'Save',
+		},
+	},
+	newInvocationBtn: 'Add invocation',
+};
