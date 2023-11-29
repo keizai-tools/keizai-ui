c85f8b19498957ba3c31a3d86ce4e0cdde35a186
Fede Estigarribia <fede.estigarribia@hotmail.com>
2023-11-29 19:03:15 -0300

    Add Enviroments in collections

--- src/common/components/Input/FunctionParameter.tsx
+++ src/common/components/Input/FunctionParameter.tsx
@@ -6,6 +6,8 @@ import { ParametersFormType } from '../Tabs/FunctionsTab/ParametersForm';
 import { Button } from '../ui/button';
 import { Input } from '../ui/input';
 
+import useEnvironments from '@/common/hooks/useEnvironments';
+import { Environment } from '@/common/types/environment';
 import { SCSpecTypeMap, isKeyOfSCSpecTypeMap } from '@/common/types/invocation';
 
 const FunctionParameterInput = ({
@@ -22,6 +24,14 @@ const FunctionParameterInput = ({
 		type: string;
 	}[];
 }) => {
+	const {
+		environments,
+		inputValue,
+		paramValue,
+		showEnvironments,
+		handleSelectEnvironment,
+		handleSearchEnvironment,
+	} = useEnvironments();
 	const valueRef = React.useRef<HTMLInputElement>(null);
 
 	const getTypeByParameterName = (parameterName: string) => {
@@ -29,6 +39,10 @@ const FunctionParameterInput = ({
 			(parameter) => parameter.name === parameterName,
 		)?.type;
 	};
+
+	const newParamValue = React.useMemo(() => {
+		return paramValue;
+	}, [paramValue]);
 
 	React.useEffect(() => {
 		if (valueRef.current) {
@@ -43,7 +57,6 @@ const FunctionParameterInput = ({
 				name={`parameters.${index}.name`}
 				render={({ field }) => {
 					const parameterType = getTypeByParameterName(field.value);
-
 					return (
 						<div className="relative min-w-[250px]">
 							<Input {...field} placeholder="Parameter key" type="text" />
@@ -59,13 +72,43 @@ const FunctionParameterInput = ({
 			<Controller
 				control={control}
 				name={`parameters.${index}.value`}
-				render={({ field }) => (
-					<Input
-						{...field}
-						ref={valueRef}
-						placeholder="Parameter value"
-						type="text"
-					/>
+				render={({ field: valueField }) => (
+					<>
+						<Input
+							{...valueField}
+							ref={valueRef}
+							placeholder="Parameter value"
+							type="text"
+							onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+								handleSearchEnvironment(e.target.value, valueField.onChange);
+							}}
+							value={inputValue}
+						/>
+						{showEnvironments && (
+							<div
+								id="dropdown"
+								className="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+							>
+								<ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
+									{environments?.map((env: Environment) => (
+										<li
+											key={env.id}
+											id={env.id}
+											className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+											onClick={(
+												e: React.MouseEvent<HTMLLIElement, MouseEvent>,
+											) => {
+												handleSelectEnvironment(e.currentTarget.id);
+												valueField.onChange(newParamValue);
+											}}
+										>
+											{env.name}: {env.value}
+										</li>
+									))}
+								</ul>
+							</div>
+						)}
+					</>
 				)}
 			/>
 
---
+++ src/common/hooks/useEnvironments.ts
@@ -0,0 +1,58 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import { useEnvironmentsQuery } from '../api/enviroments';
+import { Environment } from '../types/environment';
+
+export default function useEnvironments() {
+	const { collectionId } = useParams();
+	const { data: environments } = useEnvironmentsQuery({
+		collectionId,
+	});
+
+	const [selectEnvironment, setSelectEnvironment] =
+		React.useState<Environment | null>(null);
+	const [inputValue, setInputValue] = React.useState<string>('');
+	const [showEnvironments, setShowEnvironments] =
+		React.useState<boolean>(false);
+	const [paramValue, setParamValue] = React.useState<string>('');
+
+	const handleSelectEnvironment = (id: string) => {
+		const environment = environments?.find((env: Environment) => env.id === id);
+		if (environment) {
+			setSelectEnvironment(environment);
+			console.log(selectEnvironment);
+			setInputValue((prevValue) =>
+				prevValue
+					? prevValue.replaceAll('{', '') + `{{${environment.name}}}`
+					: `{${environment.name}}}`,
+			);
+			setParamValue((prevValue) =>
+				prevValue
+					? prevValue.replaceAll('{', '') + `${environment.value}`
+					: `${environment.value}`,
+			);
+			setShowEnvironments(false);
+		}
+	};
+
+	const handleSearchEnvironment = (
+		newSearchEnvironment: string,
+		onChange: (value: string) => void,
+	) => {
+		setShowEnvironments(newSearchEnvironment.includes('{'));
+		setInputValue(newSearchEnvironment);
+		setParamValue(newSearchEnvironment);
+		onChange(newSearchEnvironment);
+	};
+
+	return {
+		environments,
+		inputValue,
+		paramValue,
+		showEnvironments,
+		selectEnvironment,
+		handleSearchEnvironment,
+		handleSelectEnvironment,
+	};
+}
