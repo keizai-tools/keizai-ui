297d531d119058f64d0903be4b4f18ab7534e650
Leonel Tammaro <tammaroleonel@gmail.com>
2023-12-19 18:46:02 -0300

    add new method in keizai service. add new editor helper

--- src/common/constants/editor-helpers.ts
+++ src/common/constants/editor-helpers.ts
@@ -2,10 +2,12 @@ export const editorHelpers = [
 	'setEnviromentVariable',
 	'clearEnviromentVariable',
 	'clearAllEnviromentVariables',
+	'getCollectionVariableValue',
 ];
 
 export const editorHelpersDescriptions: { [key: string]: string } = {
 	setEnviromentVariable: 'Set an enviroment variable',
 	clearEnviromentVariable: 'Clear an enviroment variable',
 	clearAllEnviromentVariables: 'Clear all enviroment variables',
+	getCollectionVariableValue: 'Get the value of a collection variable',
 };
