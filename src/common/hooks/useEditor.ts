99ff3c274d4aeb451e3489af8f13cb00b45a3961
Leonel Tammaro <tammaroleonel@gmail.com>
2023-12-27 20:51:27 -0300

    add postinvocation state in custom hook

--- src/common/hooks/useEditor.ts
+++ src/common/hooks/useEditor.ts
@@ -4,22 +4,48 @@ import { useDebouncedCallback } from 'use-debounce';
 
 import { useEditPreInvocationMutation } from '@/common/api/invocations';
 
-const useEditor = (preInvocation: string) => {
-	const [editorValue, setEditorValue] = React.useState<string>(preInvocation);
+const useEditor = (preInvocation: string, postInvocation: string) => {
+	const [preInvocationEditorValue, setPreInvocationValue] =
+		React.useState<string>(preInvocation);
+	const [postInvocationEditorValue, setPostInvocationValue] =
+		React.useState<string>(postInvocation);
 	const { mutate: editInvocation } = useEditPreInvocationMutation();
 	const { invocationId } = useParams();
 
 	const preInvocationValue = useDebouncedCallback(() => {
-		editInvocation({ id: invocationId ?? '', preInvocation: editorValue });
+		preInvocation !== preInvocationEditorValue &&
+			editInvocation({
+				id: invocationId ?? '',
+				preInvocation: preInvocationEditorValue,
+			});
+	}, 700);
+
+	const postInvocationValue = useDebouncedCallback(() => {
+		postInvocation !== postInvocationEditorValue &&
+			editInvocation({
+				id: invocationId ?? '',
+				postInvocation: postInvocationEditorValue,
+			});
 	}, 700);
 
 	React.useEffect(() => {
-		if (editorValue || editorValue === '') {
+		if (preInvocationEditorValue || preInvocationEditorValue === '') {
 			preInvocationValue();
 		}
-	}, [editorValue, preInvocationValue]);
+	}, [preInvocationEditorValue, preInvocationValue]);
 
-	return { editorValue, setEditorValue };
+	React.useEffect(() => {
+		if (postInvocationEditorValue || postInvocationEditorValue === '') {
+			postInvocationValue();
+		}
+	}, [postInvocationEditorValue, postInvocationValue]);
+
+	return {
+		preInvocationEditorValue,
+		setPreInvocationValue,
+		postInvocationEditorValue,
+		setPostInvocationValue,
+	};
 };
 
 export default useEditor;
