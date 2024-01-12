4ce20ae4825f5cb4567f2ad9e737676dfb35fc17
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-01-12 10:22:42 -0300

    Add environments in ContractInput

--- src/common/components/Input/ContractInput.tsx
+++ src/common/components/Input/ContractInput.tsx
@@ -1,12 +1,14 @@
 import { Loader } from 'lucide-react';
 import React from 'react';
 
+import EnvironmentDropdownContainer from '../Environments/EnvironmentDropdownContainer';
 import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip';
 import SaveContractDialog from './SaveContractDialog';
 
 import { Button } from '@/common/components/ui/button';
 import { Input } from '@/common/components/ui/input';
 import { Select, SelectTrigger } from '@/common/components/ui/select';
+import useEnvironments from '@/common/hooks/useEnvironments';
 
 const ContractInput = ({
 	defaultValue = '',
@@ -22,11 +24,17 @@ const ContractInput = ({
 	const [contractId, setContractId] = React.useState(defaultValue);
 	const [showEditContractDialog, setShowEditContractDialog] =
 		React.useState(false);
+	const { showEnvironments, handleSelectEnvironment, handleSearchEnvironment } =
+		useEnvironments();
 
 	const handleUpdateContractId = async () => {
 		if (contractId) {
 			loadContract(contractId);
 		}
+	};
+
+	const handleSelect = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+		handleSelectEnvironment(e.currentTarget.id, setContractId);
 	};
 
 	return (
@@ -66,13 +74,21 @@ const ContractInput = ({
 						</Button>
 					</div>
 				) : (
-					<Input
-						value={contractId || ''}
-						onChange={(e) => setContractId(e.target.value)}
-						className="border-none focus-visible:ring-0"
-						placeholder="Contract address"
-						data-test="input-contract-name"
-					/>
+					<EnvironmentDropdownContainer
+						handleSelect={handleSelect}
+						showEnvironments={showEnvironments}
+					>
+						<Input
+							value={contractId || ''}
+							onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+								handleSearchEnvironment(e.target.value);
+								setContractId(e.target.value);
+							}}
+							className="border-none focus-visible:ring-0"
+							placeholder="Contract address"
+							data-test="input-contract-name"
+						/>
+					</EnvironmentDropdownContainer>
 				)}
 				{!defaultValue ? (
 					<Button
