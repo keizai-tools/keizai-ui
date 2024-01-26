c0cba53b9df02dce4138cb6fc0a39a012c89ca3e
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-01-26 16:28:14 -0300

    Add environmentInput in DropdownContainer

--- src/common/components/Environments/EnvironmentDropdownContainer.tsx
+++ src/common/components/Environments/EnvironmentDropdownContainer.tsx
@@ -1,30 +1,63 @@
+import { OnChange } from '@monaco-editor/react';
 import React from 'react';
 
+import EnvironmentInput from '../Input/EnvironmentInput';
 import EnvironmentDropdown from './EnvironmentDropdown';
 
 import useEnvironments from '@/common/hooks/useEnvironments';
 import { Environment } from '@/common/types/environment';
 
 type IProps = {
-	children: React.ReactNode;
+	value: string;
 	showEnvironments: boolean;
-	handleSelect: (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => void;
+	styles: string;
+	testName: string;
+	placeholder: string;
+	background: string;
+	fontSize: number;
+	handleChange: OnChange;
+	handleSelectEnvironment: (
+		e: React.MouseEvent<HTMLLIElement, MouseEvent>,
+	) => void;
 };
 
 function EnvironmentDropdownContainer({
-	children,
-	handleSelect,
+	value,
+	handleChange,
+	styles,
+	testName,
+	placeholder,
+	background,
+	fontSize,
+	handleSelectEnvironment,
 	showEnvironments,
 }: IProps) {
 	const { environments } = useEnvironments();
 
 	return (
 		<div id="dropdown" className="z-10 divide-slate-800 rounded shadow w-full">
-			{children}
+			<div className={`flex items-center relative w-full ${styles}`}>
+				{!value && (
+					<span
+						className="absolute left-0 top-[10px] text-sm text-slate-500 z-10 pointer-events-none px-3"
+						data-test="environment-input-placeholder"
+					>
+						{placeholder}
+					</span>
+				)}
+				<EnvironmentInput
+					value={value}
+					handleChange={handleChange}
+					background={background}
+					fontSize={fontSize}
+					testName={testName}
+					data-test={testName}
+				/>
+			</div>
 			{showEnvironments && (
 				<EnvironmentDropdown
 					environments={environments as Environment[]}
-					handleSelect={handleSelect}
+					handleSelect={handleSelectEnvironment}
 				/>
 			)}
 		</div>
