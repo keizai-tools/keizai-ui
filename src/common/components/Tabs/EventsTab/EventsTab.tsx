183d8b9ca3aa0988f7a1112336a2180da7461023
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-01-29 17:16:02 -0300

    Fix events tabs and events empty state

--- src/common/components/Tabs/EventsTab/EventsTab.tsx
+++ src/common/components/Tabs/EventsTab/EventsTab.tsx
@@ -1,20 +1,26 @@
 import EventCard from './EventCard';
 import EventEmptyState from './EventEmptyState';
 
-import { EventResponse } from '@/common/types/contract-events';
+import useContractEvents from '@/common/hooks/useContractEvents';
 
-function EventsTab({ events }: { events: EventResponse[] }) {
+function EventsTab() {
+	const { contractEvents } = useContractEvents();
+
 	return (
-		<section
-			className="w-full h-[500px] overflow-hidden overflow-y-auto scrollbar scrollbar-w-2 scrollbar-h-1 scrollbar-track-background scrollbar-thumb-slate-700 scrollbar-thumb-rounded"
-			data-test="events-tab-container"
-		>
-			{events.length > 0 ? (
-				events.map((event) => <EventCard event={event} key={event.id} />)
+		<>
+			{contractEvents.length > 0 ? (
+				<section
+					className="w-full h-[500px] overflow-hidden overflow-y-auto scrollbar scrollbar-w-2 scrollbar-h-1 scrollbar-track-background scrollbar-thumb-slate-700 scrollbar-thumb-rounded"
+					data-test="events-tab-container"
+				>
+					{contractEvents.map((event) => (
+						<EventCard event={event} key={event.id} />
+					))}
+				</section>
 			) : (
 				<EventEmptyState />
 			)}
-		</section>
+		</>
 	);
 }
 
