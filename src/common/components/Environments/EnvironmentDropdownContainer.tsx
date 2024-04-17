ef1c54acf6c60bde50c6153ba30466bcdcff5003
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-04-17 16:18:30 -0300

    Add close dropdown when click outside

--- src/common/components/Environments/EnvironmentDropdownContainer.tsx
+++ src/common/components/Environments/EnvironmentDropdownContainer.tsx
@@ -9,17 +9,41 @@ type IProps = {
 	children: React.ReactNode;
 	showEnvironments: boolean;
 	handleSelect: (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => void;
+	setShowEnvironments: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 function EnvironmentDropdownContainer({
 	children,
 	handleSelect,
 	showEnvironments,
+	setShowEnvironments,
 }: IProps) {
 	const { environments } = useEnvironments();
+	const dropdownRef = React.useRef<HTMLDivElement>(null);
+
+	React.useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				dropdownRef.current &&
+				!dropdownRef.current.contains(event.target as Node)
+			) {
+				setShowEnvironments(false);
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [setShowEnvironments, showEnvironments]);
 
 	return (
-		<div id="dropdown" className="z-10 divide-slate-800 rounded w-full">
+		<div
+			id="dropdown"
+			className="divide-slate-800 rounded w-full"
+			ref={dropdownRef}
+		>
 			{children}
 			{showEnvironments && (
 				<EnvironmentDropdown
