68e66488894e91ac5f1839cadcc8c2a27e3625a8
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-04-22 17:26:06 -0300

    Add useEndpoint in api/collections

--- src/common/api/collections.ts
+++ src/common/api/collections.ts
@@ -2,55 +2,69 @@ import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 
 import useAxios from '../hooks/useAxios';
+import { useEndpoint } from '../hooks/useEndpoint';
 import { Collection } from '../types/collection';
 
-export const useCollectionsQuery = () => {
+export const useCollectionsQuery = (teamId?: string) => {
 	const axios = useAxios();
+	const { getEndpoint } = useEndpoint();
+
+	const { apiUrl } = getEndpoint(teamId, 'collection');
 
 	const query = useQuery<Collection[]>({
 		queryKey: ['collections'],
-		queryFn: async () => axios?.get('/collection').then((res) => res.data),
+		queryFn: async () => axios?.get(apiUrl).then((res) => res.data),
 	});
 
 	return query;
 };
 
-export const useCollectionQuery = (collectionId: string | undefined) => {
+export const useCollectionQuery = (teamId?: string, collectionId?: string) => {
 	const axios = useAxios();
+
+	const { getEndpoint } = useEndpoint();
+
+	const { apiUrl } = getEndpoint(teamId, 'collection');
 
 	const query = useQuery<Collection>({
 		queryKey: ['collection', collectionId],
 		queryFn: async () =>
-			axios?.get(`/collection/${collectionId}`).then((res) => res.data),
+			axios?.get(`${apiUrl}/${collectionId}`).then((res) => res.data),
 	});
 
 	return query;
 };
 
-export const useNewCollectionMutation = () => {
+export const useNewCollectionMutation = (teamId?: string) => {
 	const queryClient = useQueryClient();
 	const navigate = useNavigate();
 	const axios = useAxios();
+	const { getEndpoint } = useEndpoint();
+
+	const { apiUrl, navUrl } = getEndpoint(teamId, 'collection');
 
 	const mutation = useMutation({
 		mutationFn: async ({ name }: { name: string }) =>
-			axios?.post('/collection', { name }).then((res) => res.data),
+			axios?.post(apiUrl, { name }).then((res) => res.data),
 		onSuccess: (data) => {
 			queryClient.invalidateQueries({ queryKey: ['collections'] });
-			navigate(`/user/collection/${data.id}`);
+			navigate(`${navUrl}/collection/${data.id}`);
 		},
 	});
 
 	return mutation;
 };
 
-export const useDeleteCollectionMutation = () => {
+export const useDeleteCollectionMutation = (teamId?: string) => {
 	const queryClient = useQueryClient();
 	const axios = useAxios();
+	const { getEndpoint } = useEndpoint();
+
+	const { apiUrl } = getEndpoint(teamId, 'collection');
 
 	const mutation = useMutation({
 		mutationFn: async (id: string) =>
-			axios?.delete(`/collection/${id}`).then((res) => res.data),
+			axios?.delete(`${apiUrl}/${id}`).then((res) => res.data),
 		onMutate: async (id) => {
 			await queryClient.cancelQueries({ queryKey: ['collections'] });
 
@@ -74,13 +88,16 @@ export const useDeleteCollectionMutation = () => {
 	return mutation;
 };
 
-export const useUpdateCollectionMutation = () => {
+export const useUpdateCollectionMutation = (teamId?: string) => {
 	const queryClient = useQueryClient();
 	const axios = useAxios();
+	const { getEndpoint } = useEndpoint();
+
+	const { apiUrl } = getEndpoint(teamId, 'collection');
 
 	const mutation = useMutation({
 		mutationFn: async ({ id, name }: { id: string; name: string }) =>
-			axios?.patch(`/collection/`, { id, name }).then((res) => res.data),
+			axios?.patch(`${apiUrl}/`, { id, name }).then((res) => res.data),
 		onMutate: async ({ id, name }) => {
 			await queryClient.cancelQueries({ queryKey: ['collections'] });
 
