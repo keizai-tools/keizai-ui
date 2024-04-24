922e2fa378ed8ccc0fefe6bb208f39045b70b623
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-04-24 14:26:27 -0300

    Add useEndpoint in api/methods

--- src/common/api/methods.ts
+++ src/common/api/methods.ts
@@ -1,14 +1,20 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
 
 import useAxios from '../hooks/useAxios';
+import { useEndpoint } from '../hooks/useEndpoint';
 import { Method } from '../types/method';
 
 export const useMethodQuery = ({ id }: { id?: string }) => {
 	const axios = useAxios();
+	const { teamId } = useParams();
+	const { getEndpoint } = useEndpoint();
+
+	const { apiUrl } = getEndpoint(teamId, 'method');
 
 	const query = useQuery<Method>({
 		queryKey: ['method', id],
-		queryFn: async () => axios?.get(`/method/${id}`).then((res) => res.data),
+		queryFn: async () => axios?.get(`${apiUrl}/${id}`).then((res) => res.data),
 		enabled: !!id,
 	});
 
@@ -18,6 +24,10 @@ export const useMethodQuery = ({ id }: { id?: string }) => {
 export const useEditParametersMethodMutation = () => {
 	const queryClient = useQueryClient();
 	const axios = useAxios();
+	const { teamId } = useParams();
+	const { getEndpoint } = useEndpoint();
+
+	const { apiUrl } = getEndpoint(teamId, 'method');
 
 	const mutation = useMutation({
 		mutationFn: async ({
@@ -30,7 +40,7 @@ export const useEditParametersMethodMutation = () => {
 			parameters: { name: string; value: string }[];
 		}) =>
 			axios
-				?.patch(`/method`, {
+				?.patch(apiUrl, {
 					id,
 					invocationId,
 					params: parameters,
