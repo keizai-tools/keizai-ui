c70c6f11b02e7e5879a09f75550018567076a69e
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-08-05 13:36:07 -0300

    Add Stellar Wallet Connection Functionality
    Introduced a new component to handle Stellar wallet connections. This component provides options to both connect and disconnect from the Stellar network. The important change is part of efforts to further enhance the user experience by simplifying the management of wallets, facilitating straightforward connections and disconnections to the network. It’s not directly related to any existing issues but is a proactive feature enhancement, aiming at improving user interaction with the Stellar network.

---
+++ src/common/components/auth/stellar/ConnectWallet.tsx
@@ -0,0 +1,41 @@
+import { Network } from 'simple-stellar-signer-api';
+
+import { Button } from '../../ui/button';
+
+import { IWallet } from '@/modules/auth/interfaces/IAuthenticationContext';
+
+interface IConnectWallet {
+	connectWallet: (network: Network) => void;
+	wallet?: IWallet | null;
+	disconnectWallet: () => void;
+	network: string;
+}
+
+function ConnectWallet({
+	wallet,
+	connectWallet,
+	disconnectWallet,
+	network,
+}: IConnectWallet) {
+	return wallet ? (
+		<Button
+			className="font-bold px-10 border-[3px] border-primary text-primary h-full hover:text-background hover:bg-primary"
+			variant="outline"
+			data-test="auth-stellar-disconnect-account-btn"
+			onClick={disconnectWallet}
+		>
+			Disconnet Account
+		</Button>
+	) : (
+		<Button
+			className="font-bold px-10 border-[3px] border-primary text-primary h-full hover:text-background hover:bg-primary"
+			variant="outline"
+			data-test="auth-stellar-connect-account-btn"
+			onClick={() => connectWallet(network.toLowerCase() as Network)}
+		>
+			Connect Account
+		</Button>
+	);
+}
+
+export default ConnectWallet;
