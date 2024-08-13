35b6be0f4ef89fc5dfef230c910087aa70c88d5d
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-08-13 18:17:28 -0300

    Introduced TypeScript interfaces for Signer API
    Enhanced type safety and code readability by introducing several TypeScript interfaces related to the Signer API. The changes encompass structured types for sign and connect messages, configuration settings, and operation groups. These definitions will facilitate feature development and maintenance for the Signer API, and ensure the expectations for data shapes are transparent and consistent across the codebase.

---
+++ src/modules/signerApi/types.d.ts
@@ -0,0 +1,44 @@
+import { MessageEventType, WalletType } from './constants/enums';
+
+export interface ISignMessage {
+	signedXDR: string;
+}
+
+export interface IConnectMessage {
+	publicKey: string;
+	wallet: WalletType;
+}
+
+export interface SimpleSignerResponse<
+	T extends ISignMessage | IConnectMessage,
+> {
+	type: MessageEventType;
+	page: string;
+	message: T;
+}
+
+export interface IOperationGroup {
+	from: number;
+	to: number;
+	title: string;
+	description: string;
+}
+
+export interface IBaseConfig {
+	url: string;
+	origin: string;
+}
+
+export interface ISignConfig extends IBaseConfig {
+	xdr: string;
+	description?: string;
+	operationGroups?: IOperationGroup[];
+}
+
+export interface IConnectConfig extends IBaseConfig {
+	wallets?: WalletType[];
+}
+
+export type ConfigType<T> = T extends ISignMessage
+	? ISignConfig
+	: IConnectConfig;
