6fc13ad03786f83ff1a40326182d2926f9557630
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-08-13 18:15:49 -0300

    Added URL retrieval functionality based on network type
    Created helper function in signer api module to improve flexibility in fetching URLs based on different network types. This ensures the right url endpoint is used for each network, increasing reliability and reducing manual url management.

---
+++ src/modules/signerApi/helpers/get-url.ts
@@ -0,0 +1,7 @@
+import NETWORKS from '../constants/networks';
+
+import { NETWORK } from '@/common/types/soroban.enum';
+
+export default function getUrl(network: Partial<NETWORK>): string | null {
+	return NETWORKS[network] ?? null;
+}
