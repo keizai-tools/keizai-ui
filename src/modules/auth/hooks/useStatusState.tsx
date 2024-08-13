ba65ae562961eac7bd7dbb67d946a8c3d6e1650e
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-08-13 12:05:21 -0300

    Refactor auth status state management
    This commit refactors the authentication status state management logic. It changes the structure of each status state object to include status, error, and loading properties instead of just a boolean. The switch statement replaces the earlier action handler pattern for easier scaling as the auth module grows. Now, new statuses like wallet and changePassword have been added, making it more versatile.
    
    This change enhances our ability to track not just the status but also loading and error states, promoting better user experiences around authentication flows. Please note that going forward, we need to pass an object to the `setStatusState` function instead of a boolean. Make sure to update any use of this function throughout our codebase.

--- src/modules/auth/hooks/useStatusState.tsx
+++ src/modules/auth/hooks/useStatusState.tsx
@@ -1,34 +1,57 @@
 import { useCallback, useReducer } from 'react';
 
-import { SET_STATUS, Action, IStatusState } from '../interfaces/IStatusState';
+import {
+	SET_STATUS,
+	Action,
+	IStatusState,
+	type IStatus,
+} from '../interfaces/IStatusState';
+
+const initial = {
+	status: false,
+	error: null,
+	loading: false,
+};
 
 const initialState: IStatusState = {
-	signIn: false,
-	signUp: false,
-	forgotPassword: false,
-	resendConfirmationCode: false,
-	confirmUser: false,
-	confirmPassword: false,
-	refreshSession: false,
-	signOut: false,
-	resetPassword: false,
+	signIn: initial,
+	signUp: initial,
+	forgotPassword: initial,
+	resendConfirmationCode: initial,
+	confirmUser: initial,
+	confirmPassword: initial,
+	refreshSession: initial,
+	signOut: initial,
+	resetPassword: initial,
+	wallet: initial,
+	changePassword: initial,
 };
 
 function reducer(state: IStatusState, action: Action): IStatusState {
-	const actionHandlers = {
-		[SET_STATUS]: () => ({ ...initialState, [action.payload]: action.value }),
-	};
-
-	const actionHandler = actionHandlers[action.type] || (() => state);
-	return actionHandler();
+	switch (action.type) {
+		case SET_STATUS:
+			return {
+				...state,
+				[action.payload]: {
+					...state[action.payload],
+					...action.value,
+				},
+			};
+		default:
+			return state;
+	}
 }
 
 export function useStatusState() {
 	const [statusState, dispatch] = useReducer(reducer, initialState);
 
 	const setStatusState = useCallback(
-		(statusType: keyof IStatusState, value: boolean) => {
-			dispatch({ type: SET_STATUS, payload: statusType, value });
+		(statusType: keyof IStatusState, value: Partial<IStatus>) => {
+			dispatch({
+				type: SET_STATUS,
+				payload: statusType,
+				value,
+			});
 		},
 		[],
 	);
