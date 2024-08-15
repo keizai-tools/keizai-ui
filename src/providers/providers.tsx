ed45c0d8a8f292dd5cdd9db36046451caf6fd166
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-08-15 16:42:44 -0300

    Refactor theme management and rename provider files

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,33 +1,35 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import flagsmith from 'flagsmith';
 import { FlagsmithProvider } from 'flagsmith/react';
-import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 import { TooltipProvider } from '../common/components/ui/tooltip';
 
 import { Toaster } from '@/common/components/ui/toaster';
-import { ThemeProvider } from '@/providers/ThemeProvider';
+import { ThemeProvider } from '@/config/theme/context/themeProvider';
+import { AuthProvider } from '@/modules/auth/context/AuthContext';
 
 const queryClient = new QueryClient();
 
-function Providers({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function Providers() {
 	return (
 		<FlagsmithProvider
 			options={{
-				environmentID: import.meta.env.VITE_FLAGSMITH_ENVIRONMENT_ID || '',
+				environmentID:
+					(import.meta.env.VITE_FLAGSMITH_ENVIRONMENT_ID as string) ?? '',
 			}}
 			flagsmith={flagsmith}
 		>
 			<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-				<QueryClientProvider client={queryClient}>
-					<TooltipProvider>
-						{children}
-						<Toaster />
-					</TooltipProvider>
-				</QueryClientProvider>
+				<AuthProvider>
+					<QueryClientProvider client={queryClient}>
+						<TooltipProvider>
+							<Outlet />
+							<Toaster />
+						</TooltipProvider>
+					</QueryClientProvider>
+				</AuthProvider>
 			</ThemeProvider>
 		</FlagsmithProvider>
 	);
 }
-
-export default Providers;
