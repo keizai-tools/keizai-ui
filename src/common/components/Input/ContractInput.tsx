5bee838616a2b54b36c14f3ad69f56dfc5418cae
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-08-15 17:21:03 -0300

    Refactor ContractInput component and enhance UI

--- src/common/components/Input/ContractInput.tsx
+++ src/common/components/Input/ContractInput.tsx
@@ -4,31 +4,28 @@ import React from 'react';
 import EnvironmentDropdownContainer from '../Environments/EnvironmentDropdownContainer';
 import EnvironmentInput from './EnvironmentInput';
 import SaveContractDialog from './SaveContractDialog';
-import SelectNetwork from './SelectNetwork';
 
 import { Button } from '@/common/components/ui/button';
 import useEnvironments from '@/common/hooks/useEnvironments';
+import { Method } from '@/common/types/method';
 
-const ContractInput = ({
+function ContractInput({
 	defaultValue = '',
 	defaultNetwork,
 	loading,
 	loadContract,
 	runInvocation,
-	prepareInvocation,
-	signedXDR,
-	walletPublicKey,
-}: {
+	method,
+}: Readonly<{
 	defaultValue: string;
 	defaultNetwork: string;
 	loading: boolean;
-	loadContract: (id: string) => Promise<void>;
+	loadContract: (contractId: string) => void;
 	runInvocation: () => void;
-	prepareInvocation: () => Promise<void>;
-	signedXDR: string;
-	walletPublicKey: string;
-}) => {
+	method?: Method;
+}>) {
 	const [contractId, setContractId] = React.useState(defaultValue);
+
 	const [showEditContractDialog, setShowEditContractDialog] =
 		React.useState(false);
 	const {
@@ -38,47 +35,61 @@ const ContractInput = ({
 		setShowEnvironments,
 	} = useEnvironments();
 
-	const handleUpdateContractId = async () => {
+	async function handleUpdateContractId() {
 		if (contractId) {
 			loadContract(contractId);
-			window.umami.track('Load contract');
+			if (window.umami) window?.umami?.track('Load contract');
 		}
-	};
+	}
 
-	const handleSelect = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+	function handleSelect(e: React.MouseEvent<HTMLLIElement, MouseEvent>) {
 		const environmentValue = handleSelectEnvironment(e.currentTarget.id);
 		setContractId(`{{${environmentValue}}}`);
-	};
+	}
 
-	const handleChange = (value: string) => {
+	function handleChange(value: string) {
 		handleSearchEnvironment(value);
 		setContractId(value);
-	};
-
-	const showRunButton = !walletPublicKey || (walletPublicKey && signedXDR);
+	}
 
 	return (
 		<div
-			className="flex items-center border p-2 rounded-md"
+			className="flex items-center gap-4 p-2 border rounded-md"
 			data-test="contract-input-container"
 		>
-			<SelectNetwork defaultNetwork={defaultNetwork} />
+			{defaultNetwork !== 'AUTO_DETECT' && (
+				<div
+					className="flex flex-col items-start gap-1 mr-2"
+					data-test="contract-input-network-container"
+				>
+					<p className="text-sm text-gray-400 select-none">Network:</p>
+					<p
+						className="text-sm font-bold text-gray-400 select-none text-primary"
+						data-test="contract-input-network"
+					>
+						{defaultNetwork}
+					</p>
+				</div>
+			)}
 			<div className="flex w-full group">
 				{defaultValue ? (
-					<div className="flex items-center justify-between flex-1 w-full relative">
+					<div className="relative flex items-center justify-between flex-1 w-full">
 						<span
-							className={defaultValue.match(/{{(.*?)}}/) ? 'text-primary' : ''}
+							className={
+								RegExp(/{{(.*?)}}/).exec(defaultValue) ? 'text-primary' : ''
+							}
 							data-test="contract-input-address"
 						>
 							{defaultValue}
 						</span>
 						<Button
 							variant="link"
-							className="invisible group-hover:visible absolute right-0 bg-background"
+							className="absolute right-0 invisible group-hover:visible bg-background"
 							data-test="btn-edit-contract-address"
 							onClick={() => {
 								setShowEditContractDialog(true);
-								window.umami.track('Open edit contract address dialog');
+								if (window.umami)
+									window?.umami?.track('Open edit contract address dialog');
 							}}
 						>
 							Edit contract address
@@ -102,43 +113,32 @@ const ContractInput = ({
 				{!defaultValue ? (
 					<Button
 						data-test="contract-input-btn-load"
-						className="transition-all"
-						disabled={loading}
+						className="w-auto px-8 py-3 font-bold transition-all duration-300 ease-in-out transform border-2 shadow-md hover:scale-105 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-background"
+						disabled={loading || !contractId}
 						onClick={() => {
 							handleUpdateContractId();
 						}}
 					>
-						{!loading ? 'LOAD' : <Loader className="animate-spin" size="14" />}
-					</Button>
-				) : showRunButton ? (
-					<Button
-						data-test="contract-input-btn-load"
-						className="transition-all"
-						onClick={runInvocation}
-						type="button"
-						disabled={loading}
-					>
 						{!loading ? (
-							'RUN'
+							'LOAD'
 						) : (
-							<div className="flex gap-1 items-center">
-								<Loader className="animate-spin" size="14" /> Running
-							</div>
+							<Loader className="w-auto font-bold animate-spin" size="20" />
 						)}
 					</Button>
 				) : (
 					<Button
 						data-test="contract-input-btn-load"
-						className="transition-all"
-						onClick={prepareInvocation}
+						className="w-auto px-4 py-3 font-bold transition-all duration-300 ease-in-out transform border-2 shadow-md hover:scale-105 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-background"
+						onClick={runInvocation}
 						type="button"
-						disabled={loading}
+						disabled={loading || !method}
 					>
 						{!loading ? (
-							'PREPARE'
+							'RUN'
 						) : (
-							<div className="flex gap-1 items-center">
-								<Loader className="animate-spin" size="14" /> Preparing
+							<div className="flex items-center gap-2">
+								<Loader className="w-auto font-bold animate-spin" size="20" />
+								<p>Running</p>
 							</div>
 						)}
 					</Button>
@@ -152,6 +152,6 @@ const ContractInput = ({
 			)}
 		</div>
 	);
-};
+}
 
 export default ContractInput;
