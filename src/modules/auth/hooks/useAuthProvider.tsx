f514dac43e184caa74324c879e8af2c22609c04a
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-08-15 17:22:44 -0300

    Corrected imported AuthContext file name casing

--- src/modules/auth/hooks/useAuthProvider.tsx
+++ src/modules/auth/hooks/useAuthProvider.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 
-import { AuthContext } from '../context/AuthContext';
+import { AuthContext } from '../context/authContext';
 
 export const useAuthProvider = () => {
 	const context = useContext(AuthContext);
