a141c0fd803301d6f018d3d62980c4ab0e9311fb
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-08-15 15:10:48 -0300

    Refactor FullscreenLoading Component from Arrow to Function Declaration

--- src/common/views/fullscreenLoading.tsx
+++ src/common/views/fullscreenLoading.tsx
@@ -1,11 +1,11 @@
 import { Loader } from 'lucide-react';
 
-const FullscreenLoading = () => {
+function FullscreenLoading() {
 	return (
 		<div className="flex items-center justify-center w-screen h-screen bg-background">
 			<Loader className="animate-spin" size="36" />
 		</div>
 	);
-};
+}
 
 export default FullscreenLoading;
