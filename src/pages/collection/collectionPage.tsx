b49a44f3b489990df0edadfe08a23bcc2150f10e
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-08-15 18:05:32 -0300

    Refactor CollectionPage component

--- src/pages/collection/collectionPage.tsx
+++ src/pages/collection/collectionPage.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 import Folders from '@/common/components/Folders/Folders';
 import { EnvironmentProvider } from '@/providers/environmentProvider';
 
-const CollectionPage = () => {
+export default function CollectionPage() {
 	return (
 		<EnvironmentProvider>
 			<main className="flex flex-1" data-test="collection-page-container">
@@ -12,6 +12,4 @@ const CollectionPage = () => {
 			</main>
 		</EnvironmentProvider>
 	);
-};
-
-export default CollectionPage;
+}
