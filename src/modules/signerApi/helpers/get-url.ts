8caae8bd6b05cea0b83aedeaa1e10db7d44ddfd6
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-08-15 14:28:43 -0300

    Remove deprecated signer API module

--- src/modules/signerApi/helpers/get-url.ts
+++
@@ -1,7 +0,0 @@
-import NETWORKS from '../constants/networks';
-
-import { NETWORK } from '@/common/types/soroban.enum';
-
-export default function getUrl(network: Partial<NETWORK>): string | null {
-	return NETWORKS[network] ?? null;
-}
