db9235a84d82029f78f2c7d81145264ed4bd456b
Fran3003 <franco.rantucho@gmail.com>
2024-09-09 14:45:18 -0300

    Add wallet integration, invocation button, and terminal visibility toggle

--- src/pages/collection/invocationbycollection.tsx
+++ src/pages/collection/invocationbycollection.tsx
@@ -1,16 +1,43 @@
+import { Loader } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { Outlet, useParams } from 'react-router-dom';
 
 import InvocationByCollectionPage from '../invocation/InvocationByCollectionPage';
 
+import Terminal from '@/common/components/ui/Terminal';
+import { Button } from '@/common/components/ui/button';
+import { Invocation } from '@/common/types/invocation';
+import { useAuthProvider } from '@/modules/auth/hooks/useAuthProvider';
+import useInvocations from '@/modules/invocation/hooks/useInvocations';
 import { InvocationService } from '@/modules/invocation/services/invocation.service';
 import { EnvironmentProvider } from '@/providers/environmentProvider';
 
 export default function InvocationByCollection() {
-	const [invocations, setInvocations] = useState<unknown[]>([]);
+	const [invocations, setInvocations] = useState<Invocation[]>([]);
 	const [loading, setLoading] = useState<boolean>(true);
 	const [error, setError] = useState<string | null>(null);
 	const { collectionId } = useParams<{ collectionId: string }>();
+	const { wallet, connectWallet } = useAuthProvider();
+
+	const [isTerminalVisible, setIsTerminalVisible] = useState(true);
+
+	const { contractResponses, handleRunInvocation, isRunningInvocation } =
+		useInvocations(invocations, wallet, connectWallet);
+
+	function toggleTerminalVisibility(event: KeyboardEvent) {
+		if (event.ctrlKey && event.key === 'j') {
+			event.preventDefault();
+			setIsTerminalVisible((prev) => !prev);
+		}
+	}
+
+	useEffect(() => {
+		window.addEventListener('keydown', toggleTerminalVisibility);
+
+		return () => {
+			window.removeEventListener('keydown', toggleTerminalVisibility);
+		};
+	}, []);
 
 	useEffect(() => {
 		const fetchInvocations = async () => {
@@ -18,10 +45,9 @@ export default function InvocationByCollection() {
 
 			try {
 				const invocationService = new InvocationService(collectionId);
-				const response: any =
+				const response: Invocation[] =
 					await invocationService.getInvocationByCollectionId();
-
-				setInvocations(response?.folders[0]?.invocations);
+				setInvocations(response);
 			} catch (err) {
 				setError('Failed to fetch invocations');
 			} finally {
@@ -34,10 +60,7 @@ export default function InvocationByCollection() {
 
 	return (
 		<EnvironmentProvider>
-			<main
-				data-test="collection-page-container"
-				className="flex flex-col space-y-6 max-h-screen overflow-y-auto p-4 w-full"
-			>
+			<main className="flex flex-col space-y-6 max-h-screen overflow-y-auto p-4 w-full">
 				{loading ? (
 					<p>Loading...</p>
 				) : error ? (
@@ -45,8 +68,22 @@ export default function InvocationByCollection() {
 				) : (
 					<div className="flex flex-col space-y-6 w-full">
 						<h2>Invocations</h2>
+						<Button
+							className="w-auto px-3 py-2 font-bold transition-all duration-300 ease-in-out transform border-2 shadow-md hover:scale-105 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-background bg-blue-500 text-white rounded-md self-end"
+							onClick={handleRunInvocation}
+							disabled={isRunningInvocation}
+						>
+							{!isRunningInvocation ? (
+								'Run All'
+							) : (
+								<div className="flex items-center gap-2">
+									<Loader className="w-auto font-bold animate-spin" size="16" />
+									<p>Running</p>
+								</div>
+							)}
+						</Button>
 						<div className="flex flex-col ">
-							{invocations?.map((invocation: any) => (
+							{invocations?.map((invocation) => (
 								<InvocationByCollectionPage
 									key={invocation.id}
 									invocation={invocation}
@@ -55,6 +92,13 @@ export default function InvocationByCollection() {
 						</div>
 					</div>
 				)}
+				<div className="relative flex flex-col w-full h-full gap-2 overflow-hidden">
+					{isTerminalVisible && (
+						<div className="flex-grow p-10">
+							<Terminal entries={contractResponses} />
+						</div>
+					)}
+				</div>
 				<Outlet />
 			</main>
 		</EnvironmentProvider>
