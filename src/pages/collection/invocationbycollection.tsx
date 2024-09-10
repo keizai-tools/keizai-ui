10827d6776b6c4c0369a98176751d6e0118bf5d7
Fran3003 <franco.rantucho@gmail.com>
2024-09-10 15:12:15 -0300

    Add execution mode switch to InvocationByCollection component; implement parallel and sequential modes

--- src/pages/collection/invocationbycollection.tsx
+++ src/pages/collection/invocationbycollection.tsx
@@ -20,9 +20,16 @@ export default function InvocationByCollection() {
 	const { wallet, connectWallet } = useAuthProvider();
 
 	const [isTerminalVisible, setIsTerminalVisible] = useState(true);
+	const [executionMode, setExecutionMode] = useState<'parallel' | 'sequential'>(
+		'parallel',
+	);
 
-	const { contractResponses, handleRunInvocation, isRunningInvocation } =
-		useInvocations(invocations, wallet, connectWallet);
+	const {
+		contractResponses,
+		handleRunInvocationSequential,
+		isRunningInvocation,
+		handleRunInvocationParallel,
+	} = useInvocations(invocations, wallet, connectWallet);
 
 	function toggleTerminalVisibility(event: KeyboardEvent) {
 		if (event.ctrlKey && event.key === 'j') {
@@ -58,6 +65,14 @@ export default function InvocationByCollection() {
 		fetchInvocations();
 	}, [collectionId]);
 
+	const handleExecution = () => {
+		if (executionMode === 'parallel') {
+			handleRunInvocationParallel();
+		} else {
+			handleRunInvocationSequential();
+		}
+	};
+
 	return (
 		<EnvironmentProvider>
 			<main className="flex flex-col space-y-6 max-h-screen overflow-y-auto p-4 w-full">
@@ -68,20 +83,39 @@ export default function InvocationByCollection() {
 				) : (
 					<div className="flex flex-col space-y-6 w-full">
 						<h2>Invocations</h2>
-						<Button
-							className="w-auto px-3 py-2 font-bold transition-all duration-300 ease-in-out transform border-2 shadow-md hover:scale-105 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-background bg-blue-500 text-white rounded-md self-end"
-							onClick={handleRunInvocation}
-							disabled={isRunningInvocation}
-						>
-							{!isRunningInvocation ? (
-								'Run All'
-							) : (
-								<div className="flex items-center gap-2">
-									<Loader className="w-auto font-bold animate-spin" size="16" />
-									<p>Running</p>
-								</div>
-							)}
-						</Button>
+						<div className="flex items-center justify-end space-x-4">
+							<select
+								className="border rounded-md p-2  bg-blue-500"
+								value={executionMode}
+								onChange={(e) =>
+									setExecutionMode(e.target.value as 'sequential' | 'parallel')
+								}
+							>
+								<option value="parallel">Parallel</option>
+								<option value="sequential">Sequential</option>
+							</select>
+							<Button
+								className="w-auto px-3 py-2 font-bold transition-all duration-300 ease-in-out transform border-2 shadow-md hover:scale-105 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-background bg-blue-500 text-white rounded-md self-end"
+								onClick={handleExecution}
+								disabled={isRunningInvocation}
+							>
+								{!isRunningInvocation ? (
+									executionMode === 'sequential' ? (
+										'Run All Sequential'
+									) : (
+										'Run All Parallel'
+									)
+								) : (
+									<div className="flex items-center gap-2">
+										<Loader
+											className="w-auto font-bold animate-spin"
+											size="16"
+										/>
+										<p>Running</p>
+									</div>
+								)}
+							</Button>
+						</div>
 						<div className="flex flex-col ">
 							{invocations?.map((invocation) => (
 								<InvocationByCollectionPage
