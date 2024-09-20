2d65f04d22b5e275aea869b7b0c0667af2355ffc
Fran3003 <franco.rantucho@gmail.com>
2024-09-20 12:06:40 -0300

    Fix button styles issues in InvocationByCollection

--- src/pages/collection/invocationbycollection.tsx
+++ src/pages/collection/invocationbycollection.tsx
@@ -19,7 +19,6 @@ export default function InvocationByCollection() {
   const { collectionId } = useParams<{ collectionId: string }>();
   const { wallet, connectWallet } = useAuthProvider();
 
-  const [isTerminalVisible, setIsTerminalVisible] = useState(true);
   const [executionMode, setExecutionMode] = useState<'parallel' | 'sequential'>(
     'parallel',
   );
@@ -31,21 +30,6 @@ export default function InvocationByCollection() {
     handleRunInvocationParallel,
     clearContractResponses,
   } = useInvocations(invocations, wallet, connectWallet);
-
-  function toggleTerminalVisibility(event: KeyboardEvent) {
-    if (event.ctrlKey && event.key === 'j') {
-      event.preventDefault();
-      setIsTerminalVisible((prev) => !prev);
-    }
-  }
-
-  useEffect(() => {
-    window.addEventListener('keydown', toggleTerminalVisibility);
-
-    return () => {
-      window.removeEventListener('keydown', toggleTerminalVisibility);
-    };
-  }, []);
 
   useEffect(() => {
     const fetchInvocations = async () => {
@@ -80,18 +64,18 @@ export default function InvocationByCollection() {
 
   return (
     <EnvironmentProvider>
-      <main className="flex flex-col space-y-6 max-h-screen overflow-y-auto p-4 w-full">
+      <main className="relative flex flex-col justify-between w-full max-h-screen gap-6 p-4 overflow-hidden">
         {loading ? (
           <p>Loading...</p>
         ) : error ? (
           <p>{error}</p>
         ) : (
-          <div className="flex flex-col space-y-6 w-full">
+          <div className="flex flex-col w-full gap-6 ">
             <div className="flex items-center justify-between p-4">
               <h2 className="text-lg font-bold">Collection Runner</h2>
               <div className="flex items-center justify-end space-x-4">
                 <select
-                  className="bg-primary rounded-lg text-primary-foreground w-auto px-6 py-2 font-bold transition-all duration-300 ease-in-out transform border-2 shadow-md hover:scale-105 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-background"
+                  className="w-auto px-6 py-2 font-bold transition-all duration-300 ease-in-out transform border-2 rounded-lg shadow-md bg-primary text-primary-foreground hover:scale-105 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-background"
                   value={executionMode}
                   onChange={(e) =>
                     setExecutionMode(
@@ -135,17 +119,11 @@ export default function InvocationByCollection() {
             </div>
           </div>
         )}
-        <div className="relative flex flex-col w-full h-full gap-2 overflow-hidden">
-          {isTerminalVisible && (
-            <div className="flex-grow p-10">
-              <Terminal
-                entries={contractResponses}
-                onClear={handleClearConsole}
-                showClearButton={true}
-              />
-            </div>
-          )}
-        </div>
+        <Terminal
+          entries={contractResponses}
+          onClear={handleClearConsole}
+          showClearButton={true}
+        />
         <Outlet />
       </main>
     </EnvironmentProvider>
