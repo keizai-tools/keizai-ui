90767487530d7b9e360ec0b9df7111108d4c7246
Fran3003 <franco.rantucho@gmail.com>
2024-12-27 18:53:14 -0300

    Adds start and stop overlay loading components
    Introduces `StartOverlayLoading` and `StopOverlayLoading` components to display loading overlays with status messages during environment initialization and termination processes.
    
    These components use intervals to update status messages and provide visual feedback with a spinning loader.

---
+++ src/common/views/StopOverlayLoading.tsx
@@ -0,0 +1,43 @@
+import { Loader } from 'lucide-react';
+import { useState, useEffect } from 'react';
+
+function StopOverlayLoading() {
+  const opacity = 0.2;
+  const [statusMessage, setStatusMessage] = useState('');
+
+  useEffect(() => {
+    const stages = [
+      'Stopping services...',
+      'Releasing resources...',
+      'Updating status...',
+      'Closing connections...',
+      'Terminating the environment...',
+    ];
+
+    let currentStage = 0;
+    const interval = setInterval(() => {
+      setStatusMessage(stages[currentStage]);
+      currentStage++;
+
+      if (currentStage >= stages.length) {
+        clearInterval(interval);
+      }
+    }, 30000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div
+      className="fixed inset-0 z-50 flex flex-col items-center justify-center backdrop-blur-sm"
+      style={{
+        backgroundColor: `hsla(222.2, 84%, 4.9%, ${opacity})`,
+      }}
+    >
+      <Loader className="animate-spin mb-4" size="36" />
+      <p className="text-white text-center">{statusMessage}</p>
+    </div>
+  );
+}
+
+export default StopOverlayLoading;
