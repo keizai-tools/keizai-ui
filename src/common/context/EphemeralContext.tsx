94569ec2a40894c49fe55b51de32a347a157e699
Fran3003 <franco.rantucho@gmail.com>
2025-01-06 16:03:18 -0300

    Adds EphemeralContext for managing ephemeral state
    Introduces EphemeralContext and EphemeralProvider to manage ephemeral state and loading status.
    Provides context hook for accessing ephemeral state and related actions.

---
+++ src/common/context/EphemeralContext.tsx
@@ -0,0 +1,44 @@
+import { createContext, ReactNode, useContext, useMemo, useState } from 'react';
+
+import { useEphemeral as useEphemeralHook } from '../hooks/useEphemeral';
+
+interface EphemeralContextType {
+  loading: boolean;
+  status: {
+    status: string;
+    taskArn: string;
+    publicIp: string;
+    isEphemeral: boolean;
+  };
+  handleStart: ({ interval }: { interval: number }) => Promise<void>;
+  handleStop: () => Promise<void>;
+  isError: boolean;
+  isLoading: boolean;
+  fetchStatus: () => Promise<void>;
+  setEphemeral: (variable: boolean) => void;
+}
+
+const EphemeralContext = createContext<EphemeralContextType | null>(null);
+
+interface EphemeralProviderProps {
+  children: ReactNode;
+}
+
+export const EphemeralProvider = ({ children }: EphemeralProviderProps) => {
+  const [loading, setLoading] = useState(false);
+  const ephemeral = useEphemeralHook(setLoading);
+  const value = useMemo(
+    () => ({ ...ephemeral, loading }),
+    [ephemeral, loading],
+  );
+
+  return (
+    <EphemeralContext.Provider value={value}>
+      {children}
+    </EphemeralContext.Provider>
+  );
+};
+
+export const useEphemeral = () => {
+  return useContext(EphemeralContext);
+};
