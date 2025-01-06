099af3101f7241cc46e9b423425d3b94c83a2064
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2025-01-06 20:54:25 -0300

    Refactors Folders component layout
    Improves readability by restructuring JSX elements
    Enhances maintainability with better indentation and spacing

--- src/common/components/Folders/Folders.tsx
+++ src/common/components/Folders/Folders.tsx
@@ -48,7 +48,10 @@ function Folders() {
 
   async function onCreateInvocation({ name }: { name: string }) {
     if (params.collectionId) {
-      createInvocation({ name, collectionId: params.collectionId });
+      createInvocation({
+        name,
+        collectionId: params.collectionId,
+      });
       if (window.umami) window?.umami?.track('Create invocation');
     }
   }
@@ -107,7 +110,7 @@ function Folders() {
       className="min-w-[250px] flex flex-col justify-between border-r dark:border-r-border h-full px-3 py-1 gap-4"
       data-test="collections-container"
     >
-      <div data-test="folders-container">
+      <div data-test="folders-container ">
         <div className="flex items-center justify-between mb-3">
           <Button
             variant="link"
@@ -137,62 +140,66 @@ function Folders() {
             </TooltipContent>
           </Tooltip>
         </div>
-        <div
-          className="flex items-center justify-between mt-3"
-          data-test="collections-header"
-        >
-          <h4
-            className="text-lg font-bold"
-            data-test="collections-header-title"
+        <div>
+          <div
+            className="flex items-center justify-between mt-3"
+            data-test="collections-header"
           >
-            Folders
-          </h4>
-          <NewEntityDialog
-            title="New folder"
-            description="Let's name your folder"
-            defaultName="Folder"
-            isLoading={isCreatingFolder}
-            onSubmit={onCreateFolder}
-            elementList={folders}
-          >
-            <Button
-              variant="link"
-              className="flex h-auto gap-1 px-0 py-1 text-xs text-slate-500 hover:text-slate-100"
-              data-test="collections-header-btn-new"
+            <h4
+              className="text-lg font-bold"
+              data-test="collections-header-title"
             >
-              <PlusIcon size={12} /> Add
-            </Button>
-          </NewEntityDialog>
-        </div>
-        {renderFoldersContent()}
-        <div
-          className="flex items-center justify-between mb-3"
-          data-test="collections-invocations-header"
-        >
-          <h4
-            className="text-lg font-bold"
-            data-test="collections-invocations-header-title"
-          >
-            Invocations
-          </h4>
-          <NewEntityDialog
-            title="New invocation"
-            description="Let's name your invocation"
-            defaultName="Invocation"
-            isLoading={isCreatingInvocation}
-            onSubmit={onCreateInvocation}
-            elementList={invocations}
-          >
-            <Button
-              variant="link"
-              className="flex h-auto gap-1 px-0 py-1 text-xs text-slate-500 hover:text-slate-100"
-              data-test="collections-header-btn-new-invocation"
+              Folders
+            </h4>
+            <NewEntityDialog
+              title="New folder"
+              description="Let's name your folder"
+              defaultName="Folder"
+              isLoading={isCreatingFolder}
+              onSubmit={onCreateFolder}
+              elementList={folders}
             >
-              <PlusIcon size={12} /> Add
-            </Button>
-          </NewEntityDialog>
+              <Button
+                variant="link"
+                className="flex h-auto gap-1 px-0 py-1 text-xs text-slate-500 hover:text-slate-100"
+                data-test="collections-header-btn-new"
+              >
+                <PlusIcon size={12} /> Add
+              </Button>
+            </NewEntityDialog>
+          </div>
+          {renderFoldersContent()}
         </div>
-        {renderInvocationsContent()}
+        <div className="mt-5 ">
+          <div
+            className="flex items-center justify-between mb-3"
+            data-test="collections-invocations-header"
+          >
+            <h4
+              className="text-lg font-bold"
+              data-test="collections-invocations-header-title"
+            >
+              Invocations
+            </h4>
+            <NewEntityDialog
+              title="New invocation"
+              description="Let's name your invocation"
+              defaultName="Invocation"
+              isLoading={isCreatingInvocation}
+              onSubmit={onCreateInvocation}
+              elementList={invocations}
+            >
+              <Button
+                variant="link"
+                className="flex h-auto gap-1 px-0 py-1 text-xs text-slate-500 hover:text-slate-100"
+                data-test="collections-header-btn-new-invocation"
+              >
+                <PlusIcon size={12} /> Add
+              </Button>
+            </NewEntityDialog>
+          </div>
+          {renderInvocationsContent()}
+        </div>
       </div>
       <div className="w-full mb-4 text-base font-semibold text-center hover:underline">
         <Link
