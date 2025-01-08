4e12046687c0728aea452464caff2af894d87956
nahuelRosas <nahuel.rosas@mi.unc.edu.ar>
2025-01-08 09:18:11 -0300

    Simplifies SelectWasmFile component
    Removes unused state and imports
    Refactors SelectTrigger to use SelectValue
    Improves accessibility and styling of select items

--- src/common/components/Input/selectWasmFile.tsx
+++ src/common/components/Input/selectWasmFile.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react';
-
-import { Button } from '../ui/button';
-
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
+  SelectValue,
 } from '@/common/components/ui/select';
 
 function SelectWasmFile({
@@ -14,17 +11,11 @@ function SelectWasmFile({
   selectedFile,
   onFileChange,
 }: Readonly<{
-  wasmFiles: { id: string; url: string }[];
+  wasmFiles: { id: string }[];
   selectedFile: string;
   onFileChange: (file: string) => void;
 }>) {
-  const [selectedFileUrl, setSelectedFileUrl] = useState<string | null>(null);
-
   const handleSelectChange = (fileId: string) => {
-    const selected = wasmFiles.find((file) => file.id === fileId);
-    if (selected) {
-      setSelectedFileUrl(selected.url);
-    }
     onFileChange(fileId);
   };
 
@@ -32,46 +23,45 @@ function SelectWasmFile({
     <div>
       <div className="flex items-center justify-between gap-2">
         <Select value={selectedFile} onValueChange={handleSelectChange}>
-          <SelectTrigger className="w-auto gap-2 px-4 py-3 font-bold border-2 rounded-md shadow-md border-slate-900 text-slate-500 focus:outline-none focus:ring-0">
-            <span
-              className={selectedFile ? 'text-slate-700' : 'text-slate-400'}
+          <SelectTrigger className="w-auto gap-2 px-4 py-3 font-bold border-2 rounded-md shadow-md border-slate-900 text-slate-500 focus:outline-none focus:ring-0 ring-0 focus-visible:ring-0 focus:ring-transparent">
+            <SelectValue
+              aria-label={selectedFile || 'Select a Wasm file'}
+              data-test="contract-input-selected-network"
+              className="flex items-center justify-between"
             >
               {selectedFile || 'Select a Wasm file'}
-            </span>
+            </SelectValue>
           </SelectTrigger>
           <SelectContent className="w-full mt-2 overflow-hidden rounded-md shadow-lg ring-1 ring-black ring-opacity-5">
             {wasmFiles.length > 0 ? (
-              wasmFiles.map(({ id }) => (
+              <>
                 <SelectItem
-                  key={id}
-                  value={id}
+                  value="Select a Wasm file"
                   className="transition-colors duration-200 cursor-pointer text-slate-700"
                 >
-                  {id}
+                  Select a Wasm file
                 </SelectItem>
-              ))
+                {wasmFiles.map(({ id }) => (
+                  <SelectItem
+                    key={id}
+                    value={id}
+                    className="transition-colors duration-200 cursor-pointer text-slate-700"
+                  >
+                    {id.trim()}
+                  </SelectItem>
+                ))}
+              </>
             ) : (
               <SelectItem
                 value=""
                 disabled
-                className="text-gray-400 cursor-not-allowed"
+                className="transition-colors duration-200 cursor-pointer text-slate-700"
               >
                 No files available
               </SelectItem>
             )}
           </SelectContent>
         </Select>
-
-        {selectedFileUrl && (
-          <a href={selectedFileUrl} target="_blank" rel="noopener noreferrer">
-            <Button
-              className="px-4 py-2 font-bold transition-all duration-300 ease-in-out transform border-2 shadow-md hover:scale-105"
-              variant="outline"
-            >
-              Download
-            </Button>
-          </a>
-        )}
       </div>
     </div>
   );
